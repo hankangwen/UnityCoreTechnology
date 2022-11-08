4a494007181683a20556b8bc5190efd80c24e783
hankangwen <13538898378@163.com>
2022-11-08 10:30:40 +0800

    Vector3 dot product and cross product.

---
+++ Assets/Demos/Chapter1_2_3VectorDotProduct/Chapter1_2_3VectorDotProduct.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class Chapter1_2_3VectorDotProduct : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject sphere;
+
+    void Start()
+    {
+        Vector3 v1 = cube.transform.position;
+        Vector3 v2 = sphere.transform.position;
+        var dir = Vector3Distance(v1, v2);
+        Debug.Log(dir);
+        dir = Vector3.Distance(v1, v2);
+        Debug.Log(dir);
+    }
+
+    // 2.向量的减法，求平方用来计算距离
+    float Vector3Distance(Vector3 a, Vector3 b)
+    {
+        float num1 = a.x - b.x;
+        float num2 = a.y - b.y;
+        float num3 = a.z - b.z;
+        return (float) Math.Sqrt((double) num1 * (double) num1 + (double) num2 * (double) num2 +
+                                 (double) num3 * (double) num3);
+    }
+}
